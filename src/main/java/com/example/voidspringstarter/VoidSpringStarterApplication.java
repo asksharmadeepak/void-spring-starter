5afd0daaa641e228377204325dafc5e375254d51
“Deepak” <deepak.asharma@springernature.com>
2021-05-23 11:24:12 +0530

    1. Setting up the base project

---
+++ src/main/java/com/example/voidspringstarter/VoidSpringStarterApplication.java
@@ -0,0 +1,13 @@
+package com.example.voidspringstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoidSpringStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoidSpringStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/voidspringstarter/VoidSpringStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.voidspringstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoidSpringStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
